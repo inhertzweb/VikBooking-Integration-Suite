d15a60b46da0f5875c9f09b0b7fddedb8b1ee295
Manuel <manuel.barbiero@silikomart.com>
2026-02-12 13:39:25 +0100

    chore: bump version to 1.1.0

--- vikbooking-integration-suite.php
+++ vikbooking-integration-suite.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: VikBooking Integration Suite
  * Description: Integrazione con VikBooking per Google Analytics e FluentCRM
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Inhertzweb Agency
  */
 
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('VB_INTEGRATION_VERSION', '1.0.0');
+define('VB_INTEGRATION_VERSION', '1.1.0');
 define('VB_INTEGRATION_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('VB_INTEGRATION_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
